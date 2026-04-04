bdc01133566473339db121adca623dc8d067e0c2
tijn <tijn@tijn.dev>
2026-04-04 21:21:00 +0200

    fix: correct variable name for serialized error in ErrorDisplay component

--- src/lib/components/ErrorDisplay.svelte
+++ src/lib/components/ErrorDisplay.svelte
@@ -4,12 +4,12 @@
 
   const { error }: { error: unknown } = $props();
 
-  const message = $derived(serializeError(error));
+  const err = $derived(serializeError(error));
 </script>
 
 <div class="rounded-lg @container bg-rose-500/20 text-rose-800-200 p-4">
   <div class="flex flex-col gap-4 @lg:flex-row">
     <CircleAlertIcon class="shrink-0" />
-    {message}
+    {err.message}
   </div>
 </div>
