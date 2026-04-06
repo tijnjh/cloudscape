750140d2f6a8dcc36bbea82d01b026f5efa8a245
tijn <tijn@tijn.dev>
2026-04-06 21:25:07 +0200

    feat: add alternative user tracks API support and implement $fetch utility

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -4,6 +4,7 @@ import { Playlist } from "$lib/schemas/playlist";
 import { Track } from "$lib/schemas/track";
 import { User } from "$lib/schemas/user";
 import { $api, getPermalinkPath } from "./utils";
+import { $fetch } from "./utils.remote";
 
 export async function resolveUser(user: string) {
   return await $api(getPermalinkPath(user), {
@@ -24,6 +25,15 @@ export async function getUserTracks({
 }: {
   id: number;
 } & Paginated) {
+  // i think soundcloud ip banned my vps, but it's only affecting this single endpoint?
+  // set VITE_ALTERNATIVE_USER_TRACKS_API=true to use another server for this endpoint
+  if (import.meta.env.VITE_ALTERNATIVE_USER_TRACKS_API === "true") {
+    const res = await $fetch({
+      input: `https://sc.maid.zone/_/api/user/${id}/tracks?pagination=limit%3D${10}%26offset%3D${offset}`,
+    });
+    return res as Collection<Track>;
+  }
+
   return await $api(`/users/${id}/tracks`, {
     params: { limit, offset },
     headers: { "Accept-Language": "en-US,en;q=0.5" },
--- src/lib/api/utils.remote.ts
+++ src/lib/api/utils.remote.ts
@@ -3,6 +3,20 @@ import { getTrackById } from "./track";
 import { upfetch } from "./utils";
 import * as v from "valibot";
 
+export const $fetch = query(
+  v.object({
+    input: v.union([v.string(), v.instance(Request), v.instance(URL)]),
+    init: v.optional(v.object({}) as v.GenericSchema<RequestInit>),
+  }),
+  async ({ input, init }) => {
+    const res = await fetch(input, init);
+
+    console.log(res);
+
+    return res.json();
+  },
+);
+
 export const scProxy = query(
   v.object({
     path: v.string(),
