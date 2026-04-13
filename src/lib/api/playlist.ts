aa37993bd8620e655f70e3b8340482d69f3d205c
tijn <tijn@tijn.dev>
2026-04-14 00:37:03 +0200

    fix: remove schema enforcement in resolvePlaylist function for flexibility with system playlists

--- src/lib/api/playlist.ts
+++ src/lib/api/playlist.ts
@@ -8,9 +8,9 @@ export function resolvePlaylist({
   user: string;
   playlist: string;
 }) {
-  return $api(getPermalinkPath(user, "sets", playlist), {
-    schema: Playlist,
-  });
+  return $api<Playlist>(getPermalinkPath(user, "sets", playlist));
+  // we don't enforce the schema here because it can also return a system playlist which differs slightly from the regular playlist schema
+  // will implement a more robust solution for this in the future
 }
 
 export function getPlaylistById(id: number) {
