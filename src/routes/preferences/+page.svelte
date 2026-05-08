681143e4baea6d79ff55b19abab08a330f3fc2ad
tijn <hoi@tijn.dev>
2026-05-08 12:24:35 +0200

    feat: improve preferences view layout

--- src/routes/preferences/+page.svelte
+++ src/routes/preferences/+page.svelte
@@ -7,7 +7,13 @@
     selectedBaseColor,
     selectedInstance,
   } from "$lib/global.svelte";
-  import { baseColors, accentColors } from "$lib/theme";
+  import {
+    baseColors,
+    accentColors,
+    type BaseColor,
+    type AccentColor,
+  } from "$lib/theme";
+  import type { PersistedState } from "runed";
 
   const selectedInstanceHostname = $derived.by(() => {
     if (!selectedInstance.current) return;
@@ -15,61 +21,50 @@
   });
 </script>
 
+{#snippet colorButton(
+  color: BaseColor | AccentColor,
+  remoteVar: PersistedState<BaseColor | AccentColor>,
+)}
+  {@const isSelected = remoteVar.current === color}
+
+  <Button
+    class={["bg-(--color) ring-black/50 ring-offset-2", isSelected && "ring-2"]}
+    style="
+            --color: var(--color-{color}-500);
+          "
+    onclick={() => {
+      remoteVar.current = color;
+    }}
+    title={color}
+  ></Button>
+{/snippet}
+
 <Main>
   {#snippet left()}
-    <Button href={resolve("/select-instance")}>Select instance</Button>
+    <h3 class="text-xl font-medium">Server instance</h3>
     current: {selectedInstanceHostname}
 
-    <h3 class="text-2xl font-medium text-accent-600-400">Theme</h3>
+    <Button href={resolve("/select-instance")}>Select instance</Button>
 
-    <h4 class="text-lg font-medium">Base Colors</h4>
+    <hr class="h-px border-0 bg-base-400-600" />
+
+    <h3 class="text-xl font-medium">Theme</h3>
+
+    <span>Base Colors</span>
 
     <div class="flex flex-wrap gap-2">
       {#each baseColors as baseColor (baseColor)}
-        {@const isSelected = selectedBaseColor.current === baseColor}
-
-        <Button
-          class={[
-            "bg-(--color) size-5 p-0 ring-black/50 ring-offset-2",
-            isSelected && "ring-2",
-          ]}
-          style="
-            --color: var(--color-{baseColor}-500);
-          "
-          onclick={() => {
-            selectedBaseColor.current = baseColor;
-          }}
-          title={baseColor}
-        ></Button>
+        {@render colorButton(baseColor, selectedBaseColor)}
       {/each}
     </div>
 
-    <h4 class="text-lg font-medium">Accent Colors</h4>
+    <span>Accent Colors</span>
 
     <div class="flex flex-wrap gap-2">
       {#each accentColors as accentColor (accentColor)}
-        {@const isSelected = selectedAccentColor.current === accentColor}
-
-        <Button
-          class={[
-            "bg-(--color) size-5 p-0 ring-black/50 ring-offset-2",
-            isSelected && "ring-2",
-          ]}
-          style="
-            --color: var(--color-{accentColor}-500);
-          "
-          onclick={() => {
-            selectedAccentColor.current = accentColor;
-          }}
-          title={accentColor}
-        ></Button>
+        {@render colorButton(accentColor, selectedAccentColor)}
       {/each}
     </div>
   {/snippet}
-  {#snippet right()}
-    <p>
-      These settings are saved in localstorage, so they will persist across
-      sessions but won't sync between devices.
-    </p>
-  {/snippet}
+  {#snippet right()}{/snippet}
 </Main>
