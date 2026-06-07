e7b558cc3909c2a585f704a23421d78296b982dd
tijn <tijn@tijn.dev>
2026-06-07 12:18:43 +0200

    refactor: rename parameter in whenInView function for clarity

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,9 @@ import { IsInViewport, watch } from "runed";
 import type { Attachment } from "svelte/attachments";
 
 export function whenInView(fn: VoidFunction): Attachment<HTMLElement> {
-  return (node) => {
-    const inViewport = new IsInViewport(() => node);
-    watch(() => inViewport.current, fn);
+  return (element) => {
+    const isInViewport = new IsInViewport(() => element);
+    watch(() => isInViewport.current, fn);
   };
 }
 
