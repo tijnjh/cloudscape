4956c08faee2ea737c9b3139583484347afa10be
tijn <tijn@tijn.dev>
2026-06-09 21:25:50 +0200

    fix: update match function type definitions for clarity and correctness

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,18 +20,21 @@ export function formatDate(date?: Date | string | null | undefined) {
   return dateFormatter.format(normalizedDate);
 }
 
+type Cases<T extends PropertyKey> = {
+  [K in T]: (value: K) => unknown;
+};
+export function match<TValue extends PropertyKey, TCases extends Cases<TValue>>(
+  value: TValue,
+  cases: TCases,
+): ReturnType<TCases[TValue]>;
 export function match<
   TValue extends PropertyKey,
-  TCases extends Record<TValue, () => unknown>,
->(value: TValue, cases: TCases): ReturnType<TCases[TValue]>;
-export function match<
-  TValue extends PropertyKey,
-  TCases extends Partial<Record<TValue, () => unknown>>,
+  TCases extends Partial<Cases<TValue>>,
 >(
   value: TValue,
-  cases: TCases & { _: () => unknown },
+  cases: TCases & { _: (value: TValue) => unknown },
 ): ReturnType<NonNullable<TCases[keyof TCases]>>;
 // @ts-expect-error - implicit any to allow for overloads
 export function match(value, cases) {
-  return (cases[value] ?? cases._)();
+  return (cases[value] ?? cases._)(value);
 }
