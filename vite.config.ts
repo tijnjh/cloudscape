c8037392e63627f05f0e2fc409e8b048baa22e1b
tijn <tijn@tijn.dev>
2026-06-11 00:12:13 +0200

    chore(vite.config): switch adapter from static to node

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
