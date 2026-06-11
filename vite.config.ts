f822e8c853155e9eb623750940eb699fc307b145
tijn <tijn@tijn.dev>
2026-06-11 11:12:02 +0200

    fix: maybe

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -7,10 +7,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     sveltekit({
-      adapter: adapter(),
-      experimental: {
-        explicitEnvironmentVariables: true,
-      },
+      adapter: adapter({ fallback: "index.html" }),
     }),
   ],
 });
