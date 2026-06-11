1339c0853b5ad3f349b1fed51190eb0ad9f3d674
tijn <tijn@tijn.dev>
2026-06-11 11:16:14 +0200

    fix: update import path for browser in layout and preferences page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { browser } from "$app/env";
+  import { browser } from "$app/environment";
   import { page } from "$app/state";
   import favicon from "$lib/assets/favicon.svg";
   import NowPlayingBar from "$lib/components/NowPlayingBar.svelte";
